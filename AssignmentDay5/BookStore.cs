b5bbde919d3684077b55bf593eaa53d45f88c02e
Manisha Mandal <mmandal@affinionds.com>
2022-07-25 09:26:29 +0530

    AssignmentDay5

---
+++ AssignmentDay5/BookStore.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentDay5
+{
+    internal class BookStore
+    {
+        public int Book_Number;
+        private  string Book_Title;
+        private  string Book_Name;
+        private  string Book_Author;
+        public int Quantity_Of_Books;
+        private  int Book_Price;
+
+        public void Update(int n)
+        {
+            Book_Number = n;
+            Console.WriteLine("Alloted Book Number:{0} \n ",n);
+            Console.WriteLine("Enter Book Tile:");
+            Book_Title = Console.ReadLine();
+            Console.WriteLine("Enter Book Name: ");
+            Book_Name = Console.ReadLine();
+            Console.WriteLine("Enter Book Author Name");
+            Book_Author = Console.ReadLine();
+            Console.WriteLine("Enter Quantity of Books:");
+            Quantity_Of_Books = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Book Price");
+            Book_Price = int.Parse(Console.ReadLine());
+
+        }
+
+        public bool check1(int n, string BookT,string  Bookname,string  BookA, int Quant)
+        {
+            if(n==Book_Number && BookT==Book_Title &&  Bookname==Book_Name && BookA==Book_Author )
+            {
+                if(Quant > Quantity_Of_Books)
+                {
+                    Console.WriteLine("Out of stock:(((");
+                    return false;
+                }
+                else
+                {
+                    Quantity_Of_Books -= Quantity_Of_Books;
+                         return true;
+                }
+            }
+            Console.WriteLine("Sorry its unavailable right now... ");
+            return false;
+        }
+
+       public void ad(int n, string BookT,string  Bookname,string  BookA, int Quant,int pr)
+        {
+            Book_Number = n;;
+            Book_Title = BookT;
+            Book_Name = Bookname;
+            Book_Author = BookA;
+            Quantity_Of_Books=Quant;
+            Book_Price=pr;
+
+        }
+        public void Show()
+        {
+            Console.Write("Book Number: {0}",Book_Number);
+            Console.Write("Book Name: {0}",Book_Name);
+            Console.Write("Book Title: {0}",Book_Title);
+            Console.Write("Book Author: {0}",Book_Author);
+            Console.Write("Quantity of books: {0}",Quantity_Of_Books);
+            Console.Write("Book Price: {0}",Book_Price);
+            Console.WriteLine();
+        }
+
+        static void Main()
+        {
+            BookStore[] bookStore1 = new BookStore[100];
+            BookStore[] purchase = new BookStore[10];
+            int n,num=0,v=0,tsm=0;
+            do
+            {
+                Console.WriteLine("Enter your Choice: ");
+                Console.WriteLine("1. Admin");
+                Console.WriteLine("2.Customer");
+                Console.WriteLine("3.Exit ");
+                n = int.Parse(Console.ReadLine());
+
+                if (bookStore1[num]==null) bookStore1[num]=new BookStore();
+
+                int x;
+
+                switch (n)
+                {
+                    case 1:
+                        Console.WriteLine("Welcome To Admin Portal \n");
+                        
+                        bookStore1[num].Update(num);
+                        num++;
+                        break;
+                    case 2:
+                        Console.WriteLine("Welcome to Jungle BookStore \n");
+                        Console.WriteLine("-------------Jungle BookStore---------");
+                        for(int i = 0; i < num; i++)
+                        {
+                            Console.WriteLine($"Book_Number: {bookStore1[i].Book_Number} Book_Name: {bookStore1[i].Book_Name} Book_Title: {bookStore1[i].Book_Title} Book_Author: {bookStore1[i].Book_Author} Price: {bookStore1[i].Book_Price}");
+
+                        }
+                        
+                        do
+                        {
+                            Console.Write("Enter Book Number: ");
+                            int bn = int.Parse(Console.ReadLine());
+                            Console.Write("Enter Book Name:");
+                            string BT = Console.ReadLine();
+                            Console.Write("Enter Book Title: ");
+                            string Bname = Console.ReadLine();
+                            Console.Write("Enter Book Author Name");
+                            string BA = Console.ReadLine();
+                            Console.Write("Enter Quantity of Books:");
+                            int Qnt = int.Parse(Console.ReadLine());
+
+                            if (bookStore1[bn].check1(bn, BT, Bname, BA, Qnt))
+                            {
+                                purchase[v] = new BookStore();
+                                purchase[v].ad(bn, BT, Bname, BA, Qnt, bookStore1[bn].Book_Price);
+                                v++;
+                                tsm += (Qnt * bookStore1[bn].Book_Price);
+                            }
+                            Console.WriteLine("Enter your choice 1 : Customer \n 0: Deline" );
+                            x = int.Parse(Console.ReadLine());
+
+                        } while (x != 0);
+                        Console.WriteLine("-----------Purchase Details-----------");
+                        for(int i = 0; i < v; i++)
+                        {
+                            purchase[i].Show();
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("Wrong Choice");
+                        break;
+
+                }
+            } while (n != 3);
+         }
+
+     }
+}
---
+++ AssignmentDay5/Furniture.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentDay5
+{
+    internal class Furniture
+    {
+        public int OrderId;
+        public DateTime OrderDate;
+        public string FurnitureType;
+        public int Qty;
+        public int TotalAmt;
+        public string PaymentMode;
+
+
+    }
+    class Cot : Furniture
+    {
+        public string CotType;
+        public string Capacity;
+        public string type;
+        public int Rate;
+
+        
+
+            public void GetData(string CotType, string Capacity, string type, int Rate)
+            {
+                this.CotType = CotType;
+                this.Capacity = Capacity;
+                this.type = type;
+                this.Rate = Rate;
+            }
+            public void ShowData()
+            {
+                Console.WriteLine("CotType: {0}", CotType);
+                Console.WriteLine("Capacity:{0}", Capacity);
+                Console.WriteLine("type: {0}", type);
+                Console.WriteLine("rate: {0}", Rate);
+            }
+
+
+        }
+    class Chair : Furniture
+    {
+        public string ChairType;
+        public string purpose;
+        public string type;
+        public int rate;
+
+        public void GetData(string ChairType,string purpose,string type,int rate)
+        {
+            this.ChairType = ChairType;
+            this.purpose = purpose;
+            this.type = type;
+            this.rate = rate;
+        }
+        public void ShowData()
+        {
+            Console.WriteLine("ChairType: {0}", ChairType);
+            Console.WriteLine("purpose:{0}",purpose);
+            Console.WriteLine("type: {0}",type);
+            Console.WriteLine("rate: {0}",rate);
+        }
+    }
+    class Demo
+    {
+        public static void Main()
+        {
+
+            Console.WriteLine("Choose Your Furniture Type: ");
+            Console.WriteLine("1. Chair");
+            Console.WriteLine("2.Cot");
+            int furnitureType=int.Parse(Console.ReadLine());
+
+            switch (furnitureType)
+            {
+                case 1:
+                    Chair chair = new Chair();
+                    Console.WriteLine("Choose your ChairType: ");
+                    Console.WriteLine("1. Wood");
+                    Console.WriteLine("2. Steel");
+                    Console.WriteLine("3. Plastic");
+                    int chairType=int.Parse(Console.ReadLine());
+                    string chairTypeValue = null;
+                    int type;
+                    string typeValue = null; ;
+                    Console.WriteLine("Choose your Purpose: ");
+                    Console.WriteLine("1. Home");
+                    Console.WriteLine("2. Office");
+                    int purpose=int.Parse(Console.ReadLine());
+                    string purposeValue;
+                    if (purpose == 1)
+                    {
+                        purposeValue = "Home";
+                    }
+                    else
+                    {
+                        purposeValue = "Office";
+                    }
+                    switch (chairType)
+                    {
+                        case 1:
+                            chairTypeValue = "Wood";
+                            Console.WriteLine("Choose your Wood type");
+                            Console.WriteLine("1.Teak Wood");
+                            Console.WriteLine("2.Rose Wood");
+                            type=int.Parse(Console.ReadLine());
+                            switch (type)
+                            {
+                                case 1:
+                                    typeValue = "Teak Wood";
+                                    break;
+                                case 2:
+                                    typeValue = "Rose Wood";
+                                    break ;
+                                default:
+                                    Console.WriteLine("Wrong choice selected");
+                                    break;
+                            }
+                            break;
+                        case 2:
+                            chairTypeValue = "Steel";
+                            Console.WriteLine("Choose your steel type");
+                            Console.WriteLine("1.Grey steel");
+                            Console.WriteLine("2.Green steel");
+                            Console.WriteLine("3.Brown Steel");
+                            type = int.Parse(Console.ReadLine());
+                            switch (type)
+                            {
+                                case 1:
+                                    typeValue = "Grey steel";
+                                    break;
+                                case 2:
+                                    typeValue = "Green Steel";
+                                    break;
+                                case 3:
+                                    typeValue = "Brown Steel";
+                                    break;
+                                default:
+                                    Console.WriteLine("Wrong choice selected");
+                                    break;
+                            }
+                            break;
+                        case 3:
+                            chairTypeValue = "Plastic";
+                            Console.WriteLine("Choose your chair color");
+                            Console.WriteLine("1.Green");
+                            Console.WriteLine("2.Red");
+                            Console.WriteLine("3.Blue");
+                            Console.WriteLine("4.White");
+                            type=int.Parse(Console.ReadLine());
+                            switch (type)
+                            {
+                                case 1:
+                                    typeValue = "Green";
+                                    break;
+                                case 2:
+                                    typeValue = "Red";
+                                    break;
+                                case 3:
+                                    typeValue = "Blue";
+                                    break;
+                                case 4:
+                                    typeValue = "White";
+                                    break ;
+                                default:
+                                    Console.WriteLine("Wrong choice selected");
+                                    break;
+                            }
+                            break;
+                        default:
+                            Console.WriteLine("Wrong choice");
+                            break;
+
+                            
+                    }
+                    Console.WriteLine("Enter the rate for chair: ");
+                    int chairRate=int.Parse(Console.ReadLine());
+                    chair.GetData(chairTypeValue, purposeValue, typeValue, chairRate);
+                    chair.ShowData();
+
+
+
+
+                break;
+                case 2:
+                    Cot cot= new Cot();
+                    Console.WriteLine("Choose your CotType: ");
+                    Console.WriteLine("1. Wood");
+                    Console.WriteLine("2. Steel");
+                    int cotType=int.Parse(Console.ReadLine());
+                    string cotTypeValue = null;
+                    typeValue = null;
+                    
+                    
+                    
+                    switch (cotType)
+                    {
+                        case 1:
+                            cotTypeValue = "Wood";
+                            Console.WriteLine("Choose your Wood type");
+                            Console.WriteLine("1.Teak Wood");
+                            Console.WriteLine("2.Rose Wood");
+                            type=int.Parse(Console.ReadLine());
+                            switch (type)
+                            {
+                                case 1:
+                                    typeValue = "Teak Wood";
+                                    break;
+                                case 2:
+                                    typeValue = "Rose Wood";
+                                    break ;
+                                default:
+                                    Console.WriteLine("Wrong choice selected");
+                                    break;
+                            }
+                            break;
+                         case 2:
+                            cotTypeValue = "Steel";
+                            Console.WriteLine("Choose your steel type");
+                            Console.WriteLine("1.Grey steel");
+                            Console.WriteLine("2.Green steel");
+                            Console.WriteLine("3.Brown Steel");
+                            type = int.Parse(Console.ReadLine());
+                            switch (type)
+                            {
+                                case 1:
+                                    typeValue = "Grey steel";
+                                    break;
+                                case 2:
+                                    typeValue = "Green Steel";
+                                    break;
+                                case 3:
+                                    typeValue = "Brown Steel";
+                                    break;
+                                default:
+                                    Console.WriteLine("Wrong choice selected");
+                                    break;
+                            }
+
+                            break;
+
+
+                    }
+                    Console.WriteLine("Enter the capacity: ");
+                    Console.WriteLine("1.Single");
+                    Console.WriteLine("2.Double");
+                    int capacity=int.Parse(Console.ReadLine());
+                    string CapacityValue = null;
+                    if (capacity == 1)
+                    {
+                        CapacityValue="Single";
+
+                    }
+                    else
+                    {
+                        CapacityValue="Double";
+                    }
+                    Console.WriteLine("Enter the rate: ");
+                    int rate=int.Parse(Console.ReadLine());
+                    cot.GetData(cotTypeValue,CapacityValue,typeValue,rate);
+                    cot.ShowData();
+
+                    
+                    break;
+                    Console.WriteLine("Enter your orderId: ");
+
+
+
+
+
+            }
+        }
+    }
+    
+}
